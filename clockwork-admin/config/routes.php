02482b2efe5a8abf827d176b8ba20712fff4751e
kmtrebacz <119664516+kmtrebacz@users.noreply.github.com>
2024-08-07 10:50:46 +0200

    feat: add log-in and sign-up to routes.php

--- clockwork-admin/config/routes.php
+++ clockwork-admin/config/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../controllers/DashboardController.php';
+require_once __DIR__ . '/../controllers/AuthController.php';
 require_once __DIR__ . '/../db/DatabaseConnection.php';
 require_once __DIR__ . '/../router/Router.php';
 
@@ -13,6 +14,16 @@ $router->get('/clockwork-admin/', function() {
 $router->get('/clockwork-admin/dashboard', function() {
     $dashboardController = new DashboardController();
     $dashboardController->render();
+});
+
+$router->get('/clockwork-admin/log-in', function() {
+    $authController = new AuthController();
+    $authController->login();
+});
+
+$router->get('/clockwork-admin/sign-up', function() {
+    $authController = new AuthController();
+    $authController->signup();
 });
 
 $router->dispatch();
